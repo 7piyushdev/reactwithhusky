668f360a8fab3e0d2c312ea662b51ec912f8e716
7piyushdev <dchlamydia@gmail.com>
2024-10-09 02:38:22 +0530

    commiting console

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  // eslint-disable-next-line no-console
+  console.log('testing');
+
   return (
     <div>
       <h1>This is the first heading...</h1>
